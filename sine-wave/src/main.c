1e54f8d6196ccb606eb91271825a4e4834e360cc
Emma Davenport <emma.v.davenport@gmail.com>
2024-12-29 23:10:09 +0000

    main: remove stupid register renaming
    just use pointers
    
    Signed-off-by: Emma Davenport <emma.v.davenport@gmail.com>

--- sine-wave/src/main.c
+++ sine-wave/src/main.c
@@ -133,62 +133,53 @@ const uint16_t sineLookupTable[] = {
 1947, 1960, 1972, 1985, 1997, 2010, 2022, 2035};
 
 #define NVIC_ISER0      (*(volatile uint32_t *)0xE000E100)
-#define TIM2_IRQn       28
-
-#define GPIOA_MODER     (*(volatile uint32_t *)(GPIOA_BASE + 0x00))
-
-#define DAC_CR          (*(volatile uint32_t *)(DAC_BASE + 0x00))
-#define DAC_DHR12R1     (*(volatile uint32_t *)(DAC_BASE + 0x08))
-
-#define GPIOB_MODER     (*(volatile uint32_t *)(GPIOB_BASE + 0x00))
-#define GPIOB_ODR       (*(volatile uint32_t *)(GPIOB_BASE + 0x14))
 
 void init_timer() {
     // Enable the TIM2 clock
-    RCC_APB1ENR |= TIM2EN;
+    RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
 
     // Set prescaler to divide the timer clock
-    TIM2_PSC = 16 - 1;  // Assuming a 16 MHz clock, this sets the timer clock to 1 MHz
+    TIM2->PSC = 16 - 1;  // Assuming a 16 MHz clock, this sets the timer clock to 1 MHz
 
     // Set auto-reload value for a desired period
-    TIM2_ARR = 44100 - 1;  // This sets the timer period to 1ms
+    TIM2->ARR = 441 - 1;  // This sets the timer period to 1ms
 
     // Enable the capture/compare channel
-    TIM2_CCER |= 1;
+    TIM2->CCER |= 1;
 
     // Enable the update interrupt
-    TIM2_DIER |= 1;  // Enable overflow interrupt
+    TIM2->DIER |= 1;  // Enable overflow interrupt
 
     // Enable the TIM2 interrupt in the NVIC
     NVIC_ISER0 |= (1 << TIM2_IRQn);
 
     // Start the timer
-    TIM2_CR1 |= 1;  // Enable the timer
+    TIM2->CR1 |= 1;  // Enable the timer
 }
 
 void init_dac() {
     // enable GPIOA
-    RCC_AHB1ENR |= 1;
+    RCC->AHB1ENR |= 1;
 
     // set PA4 to analog mode
-    GPIOA_MODER |= 0x300;
+    GPIOA->MODER |= 0x300;
 
     // enable DAC
-    RCC_APB1ENR |= 0x20000000;
+    RCC->APB1ENR |= 0x20000000;
 
     // enable DAC channel 1
-    DAC_CR |= 1;
+    DAC1->CR |= 1;
 
     // set data format but loading it into DAC_DHR12Rx right aligned 12b
-    DAC_DHR12R1 = 0x000;
+    DAC1->DHR12R1 = 0x000;
 }
 
 void init_gpio() {
     // IO port B clock enable
-    RCC_AHB1ENR |= 2;
+    RCC->AHB1ENR |= 2;
 
     // Set GPIOB pin 0 (user LED 1) to output
-    GPIOB_MODER |= 0x4000;
+    GPIOB->MODER |= 0x4000;
 
 }
 
